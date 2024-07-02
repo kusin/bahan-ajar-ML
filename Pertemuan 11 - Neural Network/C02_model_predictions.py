8e86b34c67585fb1b0c85a7d5974bd7edb4f55af
kusin <aryajayaalamsyah@gmail.com>
2024-07-02 19:03:41 +0700

    -

--- Pertemuan 11 - Neural Network/C02_model_predictions.py
+++ Pertemuan 11 - Neural Network/C02_model_predictions.py
@@ -12,7 +12,7 @@ def get_models(algorithms, timestep):
     tf.keras.backend.clear_session()
     model = tf.keras.Sequential([
       tf.keras.layers.Bidirectional(LSTM(units=50, return_sequences=True, input_shape=(timestep, 1))),
-      tf.keras.layers.Dropout(0.10),
+      tf.keras.layers.Dropout(0.05),
       tf.keras.layers.Bidirectional(LSTM(units=50, return_sequences=False)),
       tf.keras.layers.Dropout(0.05),
       tf.keras.layers.Dense(1)
@@ -23,7 +23,7 @@ def get_models(algorithms, timestep):
     tf.keras.backend.clear_session()
     model = tf.keras.Sequential([
       tf.keras.layers.Bidirectional(GRU(units=50, return_sequences=True, input_shape=(timestep, 1))),
-      tf.keras.layers.Dropout(0.10),
+      tf.keras.layers.Dropout(0.05),
       tf.keras.layers.Bidirectional(GRU(units=50, return_sequences=False)),
       tf.keras.layers.Dropout(0.05),
       tf.keras.layers.Dense(1)
