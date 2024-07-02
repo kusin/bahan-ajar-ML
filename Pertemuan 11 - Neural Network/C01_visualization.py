8a972d4e837fdb6fe935e5b8e2368c63b311a595
kusin <aryajayaalamsyah@gmail.com>
2024-07-02 08:32:55 +0700

    -

---
+++ Pertemuan 11 - Neural Network/C01_visualization.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+
+# function of lineplot
+def timeseries_matplotlib(df, nm_labels):
+
+  # create lineplot
+  fig, ax = plt.subplots(figsize = (8,4))
+  for x in range(len(nm_labels)):
+    ax.plot(df.iloc[:, 0:1], df.iloc[:, x+1:x+2], label=nm_labels[x], linewidth=2.5)
+
+  # set label-labels
+  ax.set_title("", fontsize=12)
+  ax.set_xlabel("", fontsize=10)
+  ax.set_ylabel("", fontsize=10)
+  ax.legend(loc="best")
+  ax.grid(True)
+  
+  # show lineplot
+  return plt.show()
+# ----------------------------------------------------------------------------------------
+
+# func timeseries plot
+def lineplot_matplotlib1(x1, y1, label1):
+
+  # create lineplot
+  fig, ax = plt.subplots(figsize = (8,4))
+  ax.plot(x1, y1, color="tab:blue", label=label1, linewidth=2)
+  
+  # set label-labels
+  ax.set_title("", fontsize=12)
+  ax.set_xlabel("", fontsize=10)
+  ax.set_ylabel("", fontsize=10)
+  ax.legend(loc="best")
+  ax.grid(True)
+  
+  # show lineplot
+  return plt.show()
+# ----------------------------------------------------------------------------------------
+
+# func timeseries plot
+def lineplot_matplotlib2(x1, y1, label1, x2, y2, label2):
+
+  # create lineplot
+  fig, ax = plt.subplots(figsize = (8,4))
+  ax.plot(x1, y1, color="tab:blue", label=label1, linewidth=2)
+  ax.plot(x2, y2, color="tab:red", label=label2, linewidth=2)
+  
+  
+  # set label-labels
+  ax.xaxis.set_major_formatter(DateFormatter("%Y"))
+  ax.set_title("", fontsize=12)
+  ax.set_xlabel("", fontsize=10)
+  ax.set_ylabel("", fontsize=10)
+  ax.legend(loc="best")
+  ax.grid(True)
+  
+  # show lineplot
+  return plt.show()
+# ----------------------------------------------------------------------------------------
---
+++ Pertemuan 11 - Neural Network/C02_model_predictions.py
@@ -0,0 +1,56 @@
+# lib neural network algorithms
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Bidirectional
+from keras.layers import LSTM
+from keras.layers import GRU
+from keras.layers import Dropout
+from keras.layers import Dense
+
+
+# func model predictions
+def get_models(algorithms, timestep):
+
+  # reset of session model
+  tf.keras.backend.clear_session()
+
+  # 1. SBi-LSTM-RNN architecture
+  if algorithms == "SBi-LSTM-RNN":
+    model = Sequential()
+    model.add(Bidirectional(LSTM(units=50, return_sequences=True, input_shape=(timestep, 1))))
+    model.add(Bidirectional(LSTM(units=50, return_sequences=False)))
+    model.add(Dropout(0.05))
+    model.add(Dense(1))
+  
+  # 1. SBi-GRU-RNN architecture
+  if algorithms == "SBi-GRU-RNN":
+    model = Sequential()
+    model.add(Bidirectional(GRU(units=50, return_sequences=True, input_shape=(timestep, 1))))
+    model.add(Bidirectional(GRU(units=50, return_sequences=False)))
+    model.add(Dropout(0.05))
+    model.add(Dense(1))
+  
+  # 2. compile models
+  model.compile(optimizer="adamax", loss="mean_squared_error")
+  
+  # return values
+  return model
+# ----------------------------------------------------------------------------------------
+
+# func model predictions
+def get_predictions(model, x_train, y_train, x_test, y_test):
+  
+  # 3. fitting models
+  history = model.fit(
+    x_train, y_train,
+    batch_size=16, epochs=50, verbose=0, 
+    validation_data=(x_test, y_test),
+    use_multiprocessing=False, shuffle=False
+  )
+
+  # 4. predict models
+  predictions = model.predict(x_test, verbose=0)
+
+  # return values
+  return history, predictions
+# ----------------------------------------------------------------------------------------
