ab618694b7717b51085fa25071de598c57b5ec61
kusin <aryajayaalamsyah@gmail.com>
2024-07-12 23:26:14 +0700

    -

---
+++ Pertemuan 1 - Pengantar bahasa python/Code_01_InputOutput.py
@@ -0,0 +1,26 @@
+# PROGRAM InputOutput.py
+
+# ALGORITMA
+if __name__ == "__main__":
+
+  # Input program
+  nama = "Aryajaya Alamsyah"
+  jurusan = "Teknik Informatika"
+
+  # Output program
+  print("Input Output dari suatu variabel")
+  print("Nama Anda : ", nama)
+  print("Jurusan Anda : ", jurusan)
+  print("")
+  #----------------------------------------
+
+  # Input program
+  print("Input program dari keyboard")
+  nama = input("Siapa nama anda : ")
+  jurusan = input("Apa jurusan anda : ")
+  print("")
+
+  # Output program
+  print("Output program dari keyboard")
+  print("Nama Anda : ", nama)
+  print("Jurusan Anda : ", jurusan)
